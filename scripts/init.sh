bceabd38b117d92834b2c08ce66edaa0ae01c032
Chai Phonbopit <Phonbopit@gmail.com>
2018-04-10 01:41:26 +0700

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -6,6 +6,10 @@
 # chmod +x ./init.sh
 # ./init.sh
 
+# Update postgres 9.6 that come with 17.04+ by default.
+sudo add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main"
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
 sudo apt-get update
 sudo apt-get install -y build-essential binutils-doc autoconf flex bison libjpeg-dev
 sudo apt-get install -y libfreetype6-dev zlib1g-dev libzmq3-dev libgdbm-dev libncurses5-dev
@@ -13,9 +17,7 @@ sudo apt-get install -y automake libtool libffi-dev curl git tmux gettext
 sudo apt-get install -y nginx
 sudo apt-get install -y rabbitmq-server redis-server
 sudo apt-get install -y circus
-
-sudo apt-get install -y postgresql-9.6 postgresql-contrib-9.6
-sudo apt-get install -y postgresql-doc-9.6 postgresql-server-dev-9.6
+sudo apt-get install postgresql-9.6
 
 sudo apt-get install -y python3 python3-pip python-dev python3-dev python-pip virtualenvwrapper
 sudo apt-get install -y libxml2-dev libxslt-dev
